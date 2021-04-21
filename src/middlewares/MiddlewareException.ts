6dbcd49a3430e9fc480815a107d50262f0d84c65
Joao Victor Negreiros <joaovictornsv@gmail.com>
2021-04-21 17:01:06 -0300

    feat: add celebrate error handler

--- src/middlewares/MiddlewareException.ts
+++ src/middlewares/MiddlewareException.ts
@@ -1,14 +1,35 @@
 /* eslint-disable no-unused-vars */
+import { CelebrateError } from 'celebrate';
 import {
   Request, Response, NextFunction,
 } from 'express';
 import { HttpException } from './HttpException';
 
 export default (error: Error, request: Request, response: Response, next: NextFunction) => {
+  if (error instanceof CelebrateError) {
+    const errorBody = error.details.get('body');
+    const { details } = errorBody;
+    if (!details || details.length <= 0) {
+      return response.status(400).json({
+        status: 'error',
+        message: 'Internal Celebrate Error',
+      });
+    }
+    return response.status(400).json({
+      status: 'error',
+      message: `The field '${details[0].path[0]}' ${details[0].message}`,
+    });
+  }
+
   if (error instanceof HttpException) {
     return response.status(error.statusCode).json({
-      status: error.statusCode,
+      status: 'error',
       message: error.message,
     });
   }
+  console.log(error);
+  return response.status(400).json({
+    status: 'error',
+    message: 'Internal Server Error',
+  });
 };
