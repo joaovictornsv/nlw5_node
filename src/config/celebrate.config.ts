5f796b68c08e960c94a64907d9f5e9f1353769b4
Joao Victor Negreiros <joaovictornsv@gmail.com>
2021-04-21 17:03:40 -0300

    feat: add custom error messages with celebrate

---
+++ src/config/celebrate.config.ts
@@ -0,0 +1,10 @@
+export const errorMessages = {
+  'boolean.base': 'should be a type of \'boolean\'',
+  'string.base': 'should be a type of \'text\'',
+  'string.empty': 'cannot be an empty field',
+  'string.min': 'should have a minimum length of {#limit}',
+  'string.max': 'should have a maximum length of {#limit}',
+  'string.guid': 'must be a valid uuid',
+  'string.email': 'must be a valid email',
+  'any.required': 'is a required field',
+};
