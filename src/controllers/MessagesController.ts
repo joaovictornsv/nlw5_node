2e8214afbb3247a6e29541c74cf4a0676ea413bc
Joao Victor Negreiros <joaovictornsv@gmail.com>
2021-04-21 17:03:10 -0300

    styles: invert lines and insert new line

--- src/controllers/MessagesController.ts
+++ src/controllers/MessagesController.ts
@@ -3,9 +3,9 @@ import { Request, Response } from 'express';
 
 class MessagesController {
   async create(request: Request, response: Response): Promise<Response> {
-    const messagesService = new MessagesService();
-
     const { admin_id, user_id, text } = request.body;
+
+    const messagesService = new MessagesService();
 
     const message = await messagesService.create({ admin_id, user_id, text });
 
