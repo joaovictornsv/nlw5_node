85261a5cc8fe41b21080bf35b468a0580229e196
João Victor Negreiros <joaovictornsv@gmail.com>
2021-04-21 12:22:28 -0300

    feat: add MessagesRouter in routes.ts

--- src/routes.ts
+++ src/routes.ts
@@ -1,10 +1,12 @@
 import { Router } from 'express';
 import SettingsRouter from '@routers/SettingsRouter';
 import UsersRouter from '@routers/UsersRouter';
+import MessagesRouter from '@routers/MessagesRouter';
 
 const routes = Router();
 
 routes.use('/settings', SettingsRouter);
 routes.use('/users', UsersRouter);
+routes.use('/messages', MessagesRouter);
 
 export default routes;
