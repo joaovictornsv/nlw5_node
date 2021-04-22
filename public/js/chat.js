10e93b4714c0ae94a8301d4f06cb26aaeac80942
Joao Victor Negreiros <joaovictornsv@gmail.com>
2021-04-22 12:36:18 -0300

    feat: socket emit event to client_first_access

--- public/js/chat.js
+++ public/js/chat.js
@@ -1,4 +1,25 @@
 /* eslint-disable */
+
 document.querySelector('#start_chat').addEventListener('click', (event) => {
   const socket = io();
+
+  const chat_help = document.getElementById('chat_help');
+  chat_help.style.display = 'none';
+
+  const chat_in_support = document.getElementById('chat_in_support');
+  chat_in_support.style.display = 'block';
+
+  const text = document.getElementById('txt_help').value;
+  const email = document.getElementById('email').value;
+
+  socket.on('connect', () => {
+    const params = { text, email };
+    socket.emit('client_first_access', params, (call, err) => {
+      if (err) {
+        console.error(err);
+      } else {
+        console.log(call);
+      }
+    });
+  });
 });
