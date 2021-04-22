3de608d3f2a9564469c0634b10e8ed8220c1526e
Joao Victor Negreiros <joaovictornsv@gmail.com>
2021-04-22 14:08:46 -0300

    feat: methods update and find in Settings Ctrller

--- src/controllers/SettingsController.ts
+++ src/controllers/SettingsController.ts
@@ -10,6 +10,26 @@ class SettingsController {
 
     return response.status(201).json(settings);
   }
+
+  async findByUsername(request: Request, response: Response): Promise<Response> {
+    const { username } = request.params;
+
+    const settingsService = new SettingsService();
+    const settings = await settingsService.findByUsername(username);
+
+    return response.status(200).json(settings);
+  }
+
+  async update(request: Request, response: Response): Promise<Response> {
+    const { username } = request.params;
+    const { chat } = request.body;
+
+    const settingsService = new SettingsService();
+
+    await settingsService.update(username, chat);
+
+    return response.status(200).json({ message: 'Settings updated!' });
+  }
 }
 
 export { SettingsController };
