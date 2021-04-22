dd819218987d6b25c8aa3041859600dfe101a0a4
Joao Victor Negreiros <joaovictornsv@gmail.com>
2021-04-22 14:07:29 -0300

    feat: client socket first_access ok

--- src/websockets/client.ts
+++ src/websockets/client.ts
@@ -1,7 +1,52 @@
+import { ConnectionsService } from '@services/ConnectionsService';
+import { MessagesService } from '@services/MessagesService';
+import { UsersService } from '@services/UsersService';
 import { io } from '../app';
 
+interface IParams {
+  text: string;
+  email: string;
+}
+
 io.on('connect', (socket) => {
-  socket.on('client_first_access', (params) => {
-    console.log(params);
+  const connectionsService = new ConnectionsService();
+  const usersService = new UsersService();
+  const messagesService = new MessagesService();
+
+  socket.on('client_first_access', async (params) => {
+    const socket_id = socket.id;
+    let user_id = null;
+
+    const { text, email } = params as IParams;
+
+    const userExists = await usersService.findByEmail(email);
+
+    if (!userExists) {
+      const user = await usersService.create(email);
+
+      await connectionsService.create({
+        socket_id,
+        user_id: user.id,
+      });
+
+      user_id = user.id;
+    } else {
+      user_id = userExists.id;
+      const connection = await connectionsService.findByUserId(userExists.id);
+
+      if (!connection) {
+        await connectionsService.create({
+          socket_id,
+          user_id: userExists.id,
+        });
+      } else {
+        connection.socket_id = socket.id;
+        await connectionsService.create(connection);
+      }
+    }
+
+    await messagesService.create({ text, user_id });
+
+    console.log('Connection created');
   });
 });
