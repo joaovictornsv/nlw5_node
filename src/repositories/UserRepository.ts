65c69ad72b72071089028bf93c63abd128c910ac
Joao Victor Negreiros <joaovictornsv@gmail.com>
2021-05-26 10:50:58 -0300

    feat: use interfaces in repositories

--- src/repositories/UserRepository.ts
+++ src/repositories/UserRepository.ts
@@ -1,7 +1,8 @@
 import { User } from '@entities/User';
 import { EntityRepository, Repository } from 'typeorm';
+import { IUser } from '../interfaces/entities/IUser';
 
 @EntityRepository(User)
-class UserRepository extends Repository<User> {}
+class UserRepository extends Repository<IUser> {}
 
 export { UserRepository };
