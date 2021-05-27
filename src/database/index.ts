bb6c758376a6ceca390b1808c4909ef07b04fb8a
Joao Victor Negreiros <joaovictornsv@gmail.com>
2021-05-26 23:35:47 -0300

    feat: create test connection

--- src/database/index.ts
+++ src/database/index.ts
@@ -1,6 +1,7 @@
-import { Connection, createConnection, getConnectionOptions } from 'typeorm';
+import { Connection, createConnection } from 'typeorm';
+import connections from './ormconfig.json';
 
 export default async (): Promise<Connection> => {
-  const defaultOptions = await getConnectionOptions();
-  return createConnection(defaultOptions);
+  const env = process.env.NODE_ENV || 'development';
+  return createConnection(connections[env]);
 };
